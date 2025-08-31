9b37e7d9a5c2c46375eb6273100f9b40e2fe358f
Abdur Rahman <mmarahman4847@gmail.com>
2025-08-31 17:50:06 +0530

    chore: connection string

--- packages/db/src/drizzle/db.ts
+++ packages/db/src/drizzle/db.ts
@@ -7,7 +7,7 @@ import * as schema from './index';
 // console.log(process.env.DATABASE_URL, "process.env.DATABASE_URL")
 
 const db = drizzle({
-  connection: "postgresql://neondb_owner:npg_7m4MLIckwyzU@ep-floral-surf-adwyxrzy-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connection: process.env.DATABASE_URL!,
   schema: schema,
   logger: true
 });
